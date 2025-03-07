174f9c755e739ee6b9cc2eb4125ecd75c35c771d
jaypedhadiya07 <jaypedhadiya76@gmail.com>
2025-03-07 17:58:48 +0530

    Form Project

--- vite/src/App.jsx
+++ vite/src/App.jsx
@@ -3,13 +3,17 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Home from "./Increment_Decrement_Operator/Home";
+import Test from "./TestFolder/Test"
+import Form from "./Form_Project/Form";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <Home></Home>
+      {/* <Home></Home> */}
+      {/* <Test/> */}
+      <Form></Form>
     </>
   );
 }
